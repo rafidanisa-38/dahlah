2c43869c7b8316c97056d6d0c6195a8c0ab8c286
Rafida Nisa <78836288+rafidanisa-38@users.noreply.github.com>
2023-11-22 18:34:43 +0700

    script.js

--- script.js
+++ script.js
@@ -67,35 +67,59 @@ const getNewQuote = async () =>
 getNewQuote();
 
 
+const mediaQuery1 = window.matchMedia('(min-width: 900px)')
+const mediaQuery2 = window.matchMedia('(max-width: 900px)')
 const check = document.getElementById('check');
 
+const qtitle = document.getElementById('quoteTitle');
+const qbtn = document.getElementById('qbtn');
 const qdecor1 = document.querySelector('.quote-decoration-1');
 const qdecor2 = document.querySelector('.quote-decoration-2');
 const qdecor3 = document.querySelector('.quote-decoration-3');
 const qdecor4 = document.querySelector('.quote-decoration-4');
 const qdecor5 = document.querySelector('.quote-decoration-5');
 
-check.addEventListener('click', () => {
-	check.style.display = 'none';
+function test1(e) {
+	if(e.matches) {
+		check.addEventListener('click', () => {
+			check.style.display = 'none';
+			
+			qtitle.style.display = 'none';
+			qbtn.style.display = 'none';
+			
+			qdecor1.style.transform = 'translate(-17%, -60%)';
+			qdecor1.style.transform += 'scale(.6)';
 
-	const qtitle = document.getElementById('quoteTitle');
-	qtitle.style.display = 'none';
+			qdecor2.style.transform = 'translate(-33%, -45%)';
+			qdecor2.style.transform += 'scale(.8)';
 
-	const qbtn = document.getElementById('qbtn');
-	qbtn.style.display = 'none';
-	
-	qdecor1.style.transform = 'translate(-17%, -60%)';
-	qdecor1.style.transform += 'scale(.6)';
+			qdecor3.style.transform = 'translate(10%, -50%)';
+			qdecor3.style.transform += 'scale(.7)';
 
-	qdecor2.style.transform = 'translate(-33%, -45%)';
-	qdecor2.style.transform += 'scale(.8)';
+			qdecor4.style.transform = 'translate(-20%, -30%)';
+			qdecor4.style.transform += 'scale(.6)';
 
-	qdecor3.style.transform = 'translate(10%, -50%)';
-	qdecor3.style.transform += 'scale(.7)';
+			qdecor5.style.transform = 'translate(40%, 0%)';
+			qdecor5.style.transform += 'scale(.7)';
+		})
+	}
+}
 
-	qdecor4.style.transform = 'translate(-20%, -30%)';
-	qdecor4.style.transform += 'scale(.6)';
+function test2(e) {
+	if(e.matches) {
+		check.addEventListener('click', () => {
+			check.style.display = 'none';
 
-	qdecor5.style.transform = 'translate(40%, 0%)';
-	qdecor5.style.transform += 'scale(.7)';
-})
+			qtitle.style.display = 'none';
+			qbtn.style.display = 'none';
+
+			qdecor4.style.marginTop = "33.4%";
+			qdecor5.style.marginTop = "32.4%";
+		})
+	}
+}
+
+mediaQuery1.addListener(test1)
+test1(mediaQuery1)
+mediaQuery2.addListener(test2)
+test2(mediaQuery2)
